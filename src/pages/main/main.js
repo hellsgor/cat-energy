87c65b9148f7c77895b476f2b63ddbacfef83993
hellsgor <hellsgor@gmail.com>
2022-11-21 09:49:05 +0400

    Разрабатывается блок Живой пример (промежуточный коммит)
    - создан и выведен на страницу компонент advantage-card
    - создана часть live-example главной страницы, выведена на главную
    - вызов скрипта карты перенесён в footer-big.js

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,6 @@
 'use strict'
 
 import 'Components/common/header/header.js';
-import 'Components/common/footer-big/map/map.js';
+import 'Components/common/footer-big/footer-big.js'
+
+import 'Partials/main/live-example/live-example.js'
