434b712e13bba2727a7a86ee828fc6e90f361b29
hellsgor <hellsgor@gmail.com>
2022-12-16 21:01:17 +0400

    Реализована загрузка дополнительных товаров из файла

--- src/pages/_partials/catalog/additional-goods/additional-goods.js
+++ src/pages/_partials/catalog/additional-goods/additional-goods.js
@@ -6,8 +6,6 @@ import {insertElement} from 'Utils/insert-element';
 
 const adGoodsContainer = document.querySelector('.additional-goods__goods-container');
 const adGoodCardTemplate = document.getElementById('additional-good-card-template').content;
-// const weightTemplate = document.getElementById('additional-good-card-weight').content;
-// console.log(weightTemplate);
 
 addAdditionalGood();
 function addAdditionalGood() {
@@ -15,8 +13,6 @@ function addAdditionalGood() {
   for (let i = 0; i < adGoods.length; i++) {
     let good = adGoods[i];
     let cloneCard = cloneElement(adGoodCardTemplate);
-
-
 
     cloneCard.querySelector('.additional-good-card__name').innerText = good['name'];
     cloneCard.querySelector('.additional-good-card__amount').innerText = good['amount'] + ' ' + good['units'];
@@ -27,22 +23,6 @@ function addAdditionalGood() {
 
     cloneCard.querySelector('.additional-good-card__price').innerText = good['price'] + ' ₽';
 
-
-
-
-    // if (adGoods[i]['weight']) {
-    //   console.log(adGoods[i]['weight']);
-    //   console.log(adGoods[i]['weight-units']);
-    //   console.log(cloneCard.querySelector('.additional-good-card__weight'));
-    //   // cloneCard.querySelector('.additional-good-card__weight').innerText = ;
-    // }
-
     insertElement(cloneCard, adGoodsContainer);
   }
-
-
-
-
-
-
 }
