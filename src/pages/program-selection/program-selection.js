928c657c08840b3973248ee058d8bf5639ae3955
hellsgor <hellsgor@gmail.com>
2022-12-22 18:16:11 +0400

    Реализована валидация полей и маска телефона
    - реализована самая простая валидация полей на странице "Подбор программы"
    - реализована маска для номера телефона

--- src/pages/program-selection/program-selection.js
+++ src/pages/program-selection/program-selection.js
@@ -2,3 +2,4 @@
 
 import 'Components/common/header/header.js';
 import 'Components/common/footer-big/footer-big.js';
+import 'Utils/input-tel-mask.js';
---
+++ src/utils/input-tel-mask.js
@@ -0,0 +1,70 @@
+'use strict'
+document.addEventListener("DOMContentLoaded", function () {
+  let phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+  let getInputNumbersValue = function (input) {
+    return input.value.replace(/\D/g, '');
+  }
+
+  let onPhonePaste = function (e) {
+    let input = e.target,
+      inputNumbersValue = getInputNumbersValue(input);
+    let pasted = e.clipboardData || window.clipboardData;
+    if (pasted) {
+      let pastedText = pasted.getData('Text');
+      if (/\D/g.test(pastedText)) {
+        input.value = inputNumbersValue;
+      }
+    }
+  }
+
+  let onPhoneInput = function (e) {
+    let input = e.target,
+      inputNumbersValue = getInputNumbersValue(input),
+      selectionStart = input.selectionStart,
+      formattedInputValue = "";
+
+    if (!inputNumbersValue) {
+      return input.value = "";
+    }
+
+    if (input.value.length !== selectionStart) {
+      if (e.data && /\D/g.test(e.data)) {
+        input.value = inputNumbersValue;
+      }
+      return;
+    }
+
+    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+      if (inputNumbersValue[0] === "9") inputNumbersValue = "7" + inputNumbersValue;
+      let firstSymbols = (inputNumbersValue[0] === "8") ? "8" : "+7";
+      formattedInputValue = input.value = firstSymbols + " ";
+      if (inputNumbersValue.length > 1) {
+        formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+      }
+      if (inputNumbersValue.length >= 5) {
+        formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+      }
+      if (inputNumbersValue.length >= 8) {
+        formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+      }
+      if (inputNumbersValue.length >= 10) {
+        formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+      }
+    } else {
+      formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+    }
+    input.value = formattedInputValue;
+  }
+  let onPhoneKeyDown = function (e) {
+    let inputValue = e.target.value.replace(/\D/g, '');
+    if (e.keyCode === 8 && inputValue.length === 1) {
+      e.target.value = "";
+    }
+  }
+  for (let phoneInput of phoneInputs) {
+    phoneInput.addEventListener('keydown', onPhoneKeyDown);
+    phoneInput.addEventListener('input', onPhoneInput, false);
+    phoneInput.addEventListener('paste', onPhonePaste, false);
+  }
+})
